3d757428b580a1858d26840ce08dedbaa3cf7b42
Himanshu <molkapurihimanshu@gmail.com>
2026-04-09 19:11:27 +0530

    Fix GitHub Pages deployment for Vite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
+  base: '/portfolio-1/',
   plugins: [react()],
   resolve: {
     alias: {
