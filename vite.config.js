d1957365c9033bfb17ef0c6c896b88f53290a5c7
Himanshu <molkapurihimanshu@gmail.com>
2026-06-05 17:42:35 +0530

    config: configure dynamic base path for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
-  base: '/portfolio-1/',
+  base: process.env.VERCEL ? '/' : '/portfolio-1/',
   plugins: [react()],
   resolve: {
     alias: {
